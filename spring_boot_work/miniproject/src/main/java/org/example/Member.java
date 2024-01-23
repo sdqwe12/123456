ebef953840f13dca8883b1b0bdfcaaa33f677644
sdqwe12 <dudghcjswo3@gmail.com>
2024-01-23 15:11:34 +0900

    0123 pm

--- spring_boot_work/miniproject/src/main/java/org/example/Member.java
+++ spring_boot_work/miniproject/src/main/java/org/example/Member.java
@@ -6,10 +6,10 @@ import lombok.Setter;
 
 @Setter
 @Getter // getter 메서드
-@AllArgsConstructor // 생성자
-public class Member {
-    private String email;
-    private String name;
-    private String addr;
+    @AllArgsConstructor // 생성자
+    public class Member {
+        private String email;
+        private String name;
+        private String addr;
     private String password;
 }
