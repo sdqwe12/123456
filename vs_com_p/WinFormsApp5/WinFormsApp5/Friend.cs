539d9b19e9be94d7f574a5789654f8087eb7cff0
sdqwe12 <dudghcjswo3@gmail.com>
2024-02-19 16:51:39 +0900

    20240219 16:51

---
+++ vs_com_p/WinFormsApp5/WinFormsApp5/Friend.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WinFormsApp5
+{
+    public class Friend
+    {
+        private string name;
+
+        public Friend(string name)
+        {
+            this.name = name;
+            
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+        public override string ToString()
+        {
+            return $"Friend name = {name}";
+           // return "Friend {name = "+name+"]";
+        }
+
+    }
+}
